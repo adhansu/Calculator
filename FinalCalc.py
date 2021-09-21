a1da8a1ff69f4aa893dce498301632d6290ed9b8
adhansu <86222376+adhansu@users.noreply.github.com>
2021-09-21 10:11:21 +0300

    Update FinalCalc.py

--- FinalCalc.py
+++ FinalCalc.py
@@ -1,64 +1,104 @@
 from tkinter import *
+root = Tk()
+root.geometry('500x500+450+150')
+root.title('Calculator')
+root.config(bg='#223441')
+root.resizable(width=False, height=False)
 
-# Creating frame for Calculator
-def fcalc(source, side):
-    storeObj = Frame(source, borderwidth=4, bd=4, bg="blue")
-    storeObj.pack(side=side, expand=YES, fill=BOTH)
-    return storeObj
+def button_click(number):
+    enter.insert(END, number)
 
-# Creating Button
-def button(source, side, text, command=None):
-    storeObj = Button(source, text=text, command=command)
-    storeObj.pack(side=side, expand=YES, fill=BOTH)
-    return storeObj
+def button_clear():
+    enter.delete(0, END)
 
-class app(Frame):
-    def __init__(self):
-        Frame.__init__(self)
-        self.option_add('*Font', 'Times 20 bold')
-        self.pack(expand=YES, fill=BOTH)
-        self.master.title('Calculator')
-        self.config(bg='#223441')
+def button_add():
+    first_number = enter.get()
+    global f_num
+    global math
+    math = "addition"
+    f_num = int(first_number)
+    enter.delete(0, END)
 
-        display = StringVar()
-        Entry(self, relief=RIDGE, textvariable=display,
-          justify='right'
-          , bd=20, bg="blue").pack(side=TOP,
-                                          expand=YES, fill=BOTH)
+def button_equal():
+    second_number = enter.get()
+    enter.delete(0, END)
 
-        for clearButton in (["C"]):
-            erase = fcalc(self, TOP)
-            for ichar in clearButton:
-                button(erase, LEFT, ichar, lambda
-                    storeObj = display, q=ichar: storeObj.set(''))
+    if math == "addition":
+        enter.insert(0, f_num + int(second_number))
 
-        for numButton in ("789/", "456*", "123-", "0.+"):
-            FunctionNum = fcalc(self, TOP)
-            for iEquals in numButton:
-                button(FunctionNum, LEFT, iEquals, lambda
-                    storeObj = display, q=iEquals: storeObj
-                       .set(storeObj.get() + q))
+    if math == "subtraction":
+        enter.insert(0, f_num - int(second_number))
 
-        EqualButton = fcalc(self, TOP)
-        for iEquals in "=":
-            if iEquals == '=':
-                btniEquals = button(EqualButton, LEFT, iEquals)
-                btniEquals.bind('<ButtonRelease-1>', lambda e, s=self,
-                                                            storeObj=display: s.calc(storeObj), '+')
+    if math == "multiplication":
+        enter.insert(0, f_num * int(second_number))
 
+    if math == "division":
+        enter.insert(0, f_num / int(second_number))
 
-            else:
-                btniEquals = button(EqualButton, LEFT, iEquals,
-                                    lambda storeObj=display, s=' %s ' % iEquals: storeObj.set
-                                    (storeObj.get() + s))
+def button_sub():
+    first_number = enter.get()
+    global f_num
+    global math
+    math = "subtraction"
+    f_num = int(first_number)
+    enter.delete(0, END)
 
+def button_mul():
+    first_number = enter.get()
+    global f_num
+    global math
+    math = "multiplication"
+    f_num = int(first_number)
+    enter.delete(0, END)
 
-    def calc(self, display):
-            try:
-                display.set(eval(display.get()))
-            except:
-                display.set("ERROR")
+def button_div():
+    first_number = enter.get()
+    global f_num
+    global math
+    math = "division"
+    f_num = int(first_number)
+    enter.delete(0, END)
 
-# Start the GUI
-if __name__=='__main__':
- app().mainloop()
+enter = Entry(root, width=60, font=('arial', 8, 'bold'), borderwidth=5)
+enter.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
+
+button_1 = Button(root, text="1", font=14, padx=40, pady=30, command=lambda : button_click(1))
+button_2 = Button(root, text="2", font=14, padx=40, pady=30, command=lambda : button_click(2))
+button_3 = Button(root, text="3", font=14, padx=40, pady=30, command=lambda : button_click(3))
+button_4 = Button(root, text="4", font=14, padx=40, pady=30, command=lambda : button_click(4))
+button_5 = Button(root, text="5", font=14, padx=40, pady=30, command=lambda : button_click(5))
+button_6 = Button(root, text="6", font=14, padx=40, pady=30, command=lambda : button_click(6))
+button_7 = Button(root, text="7", font=14, padx=40, pady=30, command=lambda : button_click(7))
+button_8 = Button(root, text="8", font=14, padx=40, pady=30, command=lambda : button_click(8))
+button_9 = Button(root, text="9", font=14, padx=40, pady=30, command=lambda : button_click(9))
+button_0 = Button(root, text="0", font=14, padx=40, pady=30, command=lambda : button_click(0))
+button_add = Button(root, text="+", font=14, padx=40, pady=30, command=button_add)
+button_subtract = Button(root, text="-", font=14, padx=40, pady=30, command=button_sub)
+button_multiply = Button(root, text="*", font=14, padx=40, pady=30, command=button_mul)
+button_division = Button(root, text="/", font=14, padx=40, pady=30, command=button_div)
+button_equals = Button(root, text="=", font=14, padx=40, pady=30, command=button_equal)
+button_clear = Button(root, text="C", font=14, padx=40, pady=30, command=button_clear)
+
+# put the buttons on the screen
+button_1.grid(row=3, column=0)
+button_2.grid(row=3, column=1)
+button_3.grid(row=3, column=2)
+
+button_4.grid(row=2, column=0)
+button_5.grid(row=2, column=1)
+button_6.grid(row=2, column=2)
+
+button_7.grid(row=1, column=0)
+button_8.grid(row=1, column=1)
+button_9.grid(row=1, column=2)
+
+button_0.grid(row=4, column=1)
+button_add.grid(row=4, column=3)
+button_subtract.grid(row=3, column=3)
+button_multiply.grid(row=2, column=3)
+button_division.grid(row=1, column=3)
+
+button_equals.grid(row=4, column=2)
+button_clear.grid(row=4, column=0)
+
+root.mainloop()
